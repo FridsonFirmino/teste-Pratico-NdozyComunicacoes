9ca21f9c11c85a48ab9d1e4390c32eb129b778b1
fridsonfirmnio <fridsoncmf@gmail.com>
2024-01-11 22:31:13 +0100

    feat: add image and load the font in the application

--- App.tsx
+++ App.tsx
@@ -1,8 +1,25 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
+import { ActivityIndicator } from 'react-native';
+import { 
+  useFonts, 
+  SpaceMono_400Regular,
+  SpaceMono_700Bold
+} from "@expo-google-fonts/space-mono";
+
 import { Routes } from './src/router';
 
 export default function App() {
+
+  const [LoadingFonts] = useFonts({
+    SpaceMono_400Regular,
+    SpaceMono_700Bold
+  })
+
+  if (!LoadingFonts){
+    return <ActivityIndicator size={"large"} color='#5700AF'/>
+  }
+  
   return (
     <>
       <Routes/>
